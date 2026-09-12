50d9131ca36ae81df849c36c50da3737d5c891a5
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-09-12 14:41:34 -0300

    feat: first communication

---
+++ includes/tarefa.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] !== "POST") {
+  header('Location: ../public/home/index.php');
+  die();
+}
+
+$titulo = htmlspecialchars($_POST['titulo']) ?? '';
+$descricao = htmlspecialchars($_POST['descricao']) ?? '';
+$responsavel = htmlspecialchars($_POST['responsavel']) ?? '';
+$errors = [];
+
+if (!isset($titulo) || !isset($descricao) || !isset($responsavel)) {
+  $errors['ParametrosNaoEncontrados'] = 'Passe todos os parâmetros para criar a tarefa';
+}
+
+if (empty($titulo) || empty($descricao) || empty($responsavel)) {
+  $errors['InputsVazios'] = 'Preencha todos os campos para criar a tarefa';
+}
+
+if (!empty($errors)) {
+  $_SESSION['ERRORS'] = $errors;
+  header('Location: ../public/home/index.php');
+  die();
+}
+
+header('Location: ../public/home/index.php?created=success');
---
+++ includes/tratarErros.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+function getErrors()
+{
+  if (!isset($_SESSION['ERRORS'])) return [];
+
+  return $_SESSION['ERRORS'] ?? [];
+}
+
+function mostrarErros()
+{
+  $errors = getErrors();
+
+  foreach ($errors as $err) {
+    echo "<p class='text-danger'>" . $err . "<p>";
+  }
+
+  unset($_SESSION['ERRORS']);
+}
+
+function exibirMensagem()
+{
+  if (!empty(getErrors())) {
+    mostrarErros();
+    return;
+  }
+  if (isset($_GET['created']) && $_GET['created'] === 'success') {
+    echo "<p class='text-success'>Tarefa criada com sucesso!</p>";
+  }
+}
